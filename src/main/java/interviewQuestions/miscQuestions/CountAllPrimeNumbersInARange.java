8d052ca02f031f94c718ade76aad6c5583b026ed
Vishal Soni <sonivishal238@gmail.com>
2025-12-25 16:18:23 +0530

    primes

---
+++ src/main/java/interviewQuestions/miscQuestions/CountAllPrimeNumbersInARange.java
@@ -0,0 +1,33 @@
+package interviewQuestions.miscQuestions;
+
+public class CountAllPrimeNumbersInARange {
+    public static void main(String[] args) {
+
+        /*
+            Sieve Algo
+            Assume all are prime and mark them as true
+            0, 1 are not prime by default
+         */
+
+        int range = 50;
+        int count = 0;
+        boolean primes[] = new boolean[range+1];
+
+        for (int i = 2; i < primes.length; i++) {
+            primes[i] = true;
+        }
+
+        for(int i = 2; i < range; i++){
+            if(primes[i] == true){
+                count++;
+            }
+
+            for(int j = 2*i; j < range; j=j+i){
+                primes[j] = false;
+            }
+
+        }
+
+        System.out.println(count);
+    }
+}
---
+++ src/main/java/language/ArraysOperations.java
@@ -0,0 +1,8 @@
+package language;
+
+public class ArraysOperations {
+
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 2,3, 4, 5};
+    }
+}
