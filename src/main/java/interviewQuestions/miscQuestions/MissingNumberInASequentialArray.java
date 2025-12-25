ca50fbf0ecca649419c121f444618fa5811f9aea
Vishal Soni <sonivishal238@gmail.com>
2025-12-25 16:35:52 +0530

    questions

---
+++ src/main/java/interviewQuestions/miscQuestions/MissingNumberInASequentialArray.java
@@ -0,0 +1,24 @@
+package interviewQuestions.miscQuestions;
+
+public class MissingNumberInASequentialArray {
+    public static void main(String[] args) {
+
+        int array[] = {1,2,3,4,5,7,8,9,10};
+
+        int actual = 0;
+        System.out.println("Length: " + array.length);
+        for(int i=0; i<array.length; i++) {
+            actual = actual + array[i];
+        }
+
+        int expected = 0;
+        System.out.println("Actual: " + actual);
+
+        for(int j = 1; j<=array.length+1; j++) {
+            expected = expected + j;
+        }
+
+        System.out.println("Expected: " + expected);
+        System.out.println("Missing: " + (expected-actual));
+    }
+}
