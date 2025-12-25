f5fbf54c14d0f55e9b9568b6851cd9ea53ba02aa
Vishal Soni <sonivishal238@gmail.com>
2025-12-25 19:54:47 +0530

    Update ArraysOperations.java

--- src/main/java/language/ArraysOperations.java
+++ src/main/java/language/ArraysOperations.java
@@ -1,8 +1,33 @@
 package language;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArraysOperations {
 
     public static void main(String[] args) {
         int arr[] = {0, 1, 2,3, 4, 5};
+        insertionInAnArrayAtASpecificIndex(arr, 3, 20);
+    }
+
+    public static void insertionInAnArrayAtASpecificIndex(int[] arr, int index, int value) {
+        /*
+        1. Case when array has empty places
+        2. When it does not have any empty space
+         */
+
+        for(int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i-1];
+        }
+
+        arr[index] = value;
+
+        System.out.println("array after inserting " + value + " at index " + index + " ");
+        printArray(arr);
+    }
+
+    static void printArray(int arr[]) {
+        Arrays.stream(arr).forEach(num -> System.out.print(num + " "));
+        System.out.println();
     }
 }
