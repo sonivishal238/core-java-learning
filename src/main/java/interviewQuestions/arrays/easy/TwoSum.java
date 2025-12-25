e868531b01a8c48bc134cb9a7c2b065ef9fc493a
Vishal Soni <sonivishal238@gmail.com>
2025-12-25 18:01:23 +0530

    Create TwoSum.java

---
+++ src/main/java/interviewQuestions/arrays/easy/TwoSum.java
@@ -0,0 +1,32 @@
+package interviewQuestions.arrays.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int nums[] = {2, 11, 7, 25, 1, 56, 78, 9, 10};
+
+        for(int num : getTwoSums(nums, 100)) {
+            System.out.println(num);
+        }
+    }
+
+    static int[] getTwoSums(int[] nums, int key) {
+        Map<Integer, Integer> results = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int comp = key - nums[i];
+
+            if(results.containsKey(comp)) {
+                return new int[] {results.get(comp), i};
+            }
+
+            results.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+
+}
