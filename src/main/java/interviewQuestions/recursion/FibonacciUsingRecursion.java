08fbc8022d2102dae2fa387926a167fa88c98b7b
Vishal Soni <sonivishal238@gmail.com>
2026-01-05 18:43:03 +0530

    Create FibonacciUsingRecursion.java

---
+++ src/main/java/interviewQuestions/recursion/FibonacciUsingRecursion.java
@@ -0,0 +1,25 @@
+package interviewQuestions.recursion;
+
+import org.testng.annotations.Test;
+
+public class FibonacciUsingRecursion {
+
+    public static void main(String[] args) {
+        int range = 10;
+        int maxValue = 30;
+            int member = getNextFib(i);
+//            if(member > maxValue)
+//                break;   uncomment this to print less than a max value
+            System.out.print( member + " ");
+        }
+    }
+
+    static int getNextFib(int n) {
+        if(n <= 1) {
+            return n;
+        }
+
+        return getNextFib(n - 1) + getNextFib(n - 2);
+    }
+
+}
