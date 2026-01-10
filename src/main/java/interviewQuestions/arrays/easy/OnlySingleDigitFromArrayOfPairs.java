2b790983cb89b8872c962f119684763afb3b5107
Vishal Soni <sonivishal238@gmail.com>
2026-01-10 17:30:25 +0530

    more

---
+++ src/main/java/interviewQuestions/arrays/easy/OnlySingleDigitFromArrayOfPairs.java
@@ -0,0 +1,22 @@
+package interviewQuestions.arrays.easy;
+
+public class OnlySingleDigitFromArrayOfPairs {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Array hai jisme saare elements pair me hai and only 1 hi element single hai, find single
+		 */
+		
+		int[] arr = {1,4,1,6,6,4,7};
+		int ans = 0;
+		
+		for(int num : arr) {
+			ans = ans^num;
+		}
+		
+		System.out.println(ans);
+		
+	}
+
+}
---
+++ src/main/java/interviewQuestions/binarysearchquestions/PivotLeetCode.java
@@ -0,0 +1,33 @@
+package interviewQuestions.binarysearchquestions;
+
+public class PivotLeetCode {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,3};
+
+		System.out.println("pivot is:" + pivotIndex(arr));
+	}
+
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+
+        // Calculate the total sum of the array
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // Iterate through the array to find the pivot index
+        for (int i = 0; i < nums.length; i++) {
+            // Check if the sum of elements to the left is equal to the sum of elements to the right
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; // Found the pivot index
+            }
+
+            // Update the sum of elements to the left
+            leftSum += nums[i];
+        }
+
+        return -1; // No pivot index found
+    }
+}
