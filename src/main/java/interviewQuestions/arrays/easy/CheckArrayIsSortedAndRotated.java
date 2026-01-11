d5ce29f76c82087907495a555b0c416ed8b9ed53
Vishal Soni <sonivishal238@gmail.com>
2026-01-11 14:16:14 +0530

    more

--- src/main/java/interviewQuestions/arrays/easy/CheckArrayIsSortedAndRotated.java
+++ src/main/java/interviewQuestions/arrays/easy/CheckArrayIsSortedAndRotated.java
@@ -10,19 +10,16 @@ public class CheckArrayIsSortedAndRotated {
 	}
 
 	static boolean isSortedRotated(int[] arr) {
-		
 		int count = 0;
 		int size = arr.length;
 		
 		for(int i = 1; i < size; i++) {
-			if(arr[i-1] > arr[i]) {
-				count++;
-			}
-		}
-		
-		if(arr[size-1] > arr[0])
-			count++;
-		
-		return count<=1;
+            if(arr[i-1] > arr[i]) {
+                count++;
+            }
+        }
+        if(arr[size-1] > arr[0]) count++;
+
+        return count <=1;
 	}
 }
---
+++ src/main/java/interviewQuestions/arrays/medium/SumOfTwoArrays.java
@@ -0,0 +1,55 @@
+package interviewQuestions.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SumOfTwoArrays {
+	public static int[] findArraySum(int[] a, int n, int[] b, int m) {
+        int i = n-1;
+        int j = m-1;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int carry = 0;
+
+        while(i>=0 && j>=0){
+            
+            int sum = a[i] + b[j] + carry;
+
+            // real life me carry ese hi nikalta hai
+            carry = sum/10;
+            
+            // real life me sum ese hi nikalta hai, krke dekh
+            sum = sum%10;
+            ans.add(sum);
+            i--;
+            j--;
+        }
+
+        while(i>=0){
+            int sum = a[i] + carry;
+            carry = sum/10;
+            sum = sum%10;
+            ans.add(sum);
+            i--;        
+        }
+
+        while(j>=0){
+            int sum = b[j] + carry;
+            carry = sum/10;
+            sum = sum%10;
+            ans.add(sum);
+            j--;        
+        }
+
+        if(carry != 0){
+            ans.add(carry);
+        }
+
+        Collections.reverse(ans);
+        int[] result = new int[ans.size()];
+        for(int k = 0; k < ans.size(); k++){
+            result[k] = ans.get(k);
+        }
+
+        return result;
+    }
+}
