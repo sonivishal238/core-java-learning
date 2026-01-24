8a244b45bd6e637d4131101d8f522f4328a4f698
sonivishal238 <68811345+sonivishal238@users.noreply.github.com>
2026-01-24 20:17:29 +0530

    Update RightAndLeftShiftArray.java

--- src/main/java/interviewQuestions/arrays/medium/RightAndLeftShiftArray.java
+++ src/main/java/interviewQuestions/arrays/medium/RightAndLeftShiftArray.java
@@ -15,34 +15,62 @@ public class RightAndLeftShiftArray {
 	}
 
 	static int[] ShiftRight(int[] arr, int k) {
-		
 		int n = arr.length;
-		if(k == n) {
-			return arr;
-		}
+		if(k == n) return arr;
 		
 		int temp[] = new int[n];
-		
 		for(int i = 0; i < n; i++) {
 			temp[(i+k)%n] = arr[i];
 		}
-		
 		return temp;
 	}
-	
+
 	static int[] LeftShift(int[] arr, int k) {
 		int n = arr.length;
-		
-		if(k == n) {
-			return arr;
-		}
-		
+		if(k == n) return arr;
 		int temp[] = new int[n];
-		
 		for(int i = 0; i < n; i++) {
 			temp[(i+n-k)%n] = arr[i];
 		}
-		
 		return temp;
 	}
+
+	static void rightShiftConstantSpace(int[] arr, int k) {
+		int n = arr.length;
+		k = k % n; // handle k > n
+
+		/* Steps:
+		1. Reverse the whole array
+		2. Reverse first k elements
+		3. Reverse remaining n - k elements
+		 */
+		reverse(arr, 0, n - 1);
+		reverse(arr, 0, k - 1);
+		reverse(arr, k, n - 1);
+	}
+
+	static void leftShiftConstantSpace(int[] arr, int k) {
+		int n = arr.length;
+		k = k % n; // handle k > n
+
+		/* Steps:
+		1. Reverse first k elements
+		2. Reverse remaining n - k elements
+		3. Reverse the whole array
+		 */
+		reverse(arr, 0, n - 1);
+		reverse(arr, 0, k - 1);
+		reverse(arr, k, n - 1);
+	}
+
+	static void reverse(int[] arr, int start, int end) {
+		while (start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
 }
