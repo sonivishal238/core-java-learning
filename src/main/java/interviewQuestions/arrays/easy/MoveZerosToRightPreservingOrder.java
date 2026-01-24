d02a3306d0bee00dfcbc4696e08c4fe73287a940
sonivishal238 <68811345+sonivishal238@users.noreply.github.com>
2026-01-24 19:46:41 +0530

    Update MoveZerosToRightPreservingOrder.java

--- src/main/java/interviewQuestions/arrays/easy/MoveZerosToRightPreservingOrder.java
+++ src/main/java/interviewQuestions/arrays/easy/MoveZerosToRightPreservingOrder.java
@@ -8,14 +8,14 @@ public class MoveZerosToRightPreservingOrder {
     public void solution(){
         int[] arr = {0, 2, 0, 1, 2, 4, 0, 8, 0, 9, 0, 89, 11, 0, 56};
 
-        int nonZero =0;
+        int positionOfLastNonZero =0;
 
         for(int i =0; i < arr.length; i++) {
             if(arr[i] != 0) {
-                int temp = arr[nonZero];
-                arr[nonZero] = arr[i];
+                int temp = arr[positionOfLastNonZero];
+                arr[positionOfLastNonZero] = arr[i];
                 arr[i] = temp;
-                nonZero++;
+                positionOfLastNonZero++;
 
             }
         }
