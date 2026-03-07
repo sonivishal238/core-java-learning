1de3355655200904afee9d230dbef9a0e12f44bd
sonivishal238 <68811345+sonivishal238@users.noreply.github.com>
2026-03-07 15:01:52 +0530

    Update MinSizeSubarraySum.java

--- src/main/java/problemSolving/DSApatterns/slidingWindow/MinSizeSubarraySum.java
+++ src/main/java/problemSolving/DSApatterns/slidingWindow/MinSizeSubarraySum.java
@@ -26,9 +26,12 @@ public class MinSizeSubarraySum {
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
 
-            while (sum >= target) {
-                minLen = Math.min(minLen, right - left + 1);
-                sum -= nums[left++];
+            while(sum >= target) {
+                // Calculate the length
+                minLen = Math.min(minLen, right-left+1);
+
+                // remove the outgoing;
+                sum-=nums[left++];
             }
         }
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
